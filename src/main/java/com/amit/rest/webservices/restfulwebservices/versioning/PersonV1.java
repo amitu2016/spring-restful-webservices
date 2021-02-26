de9c5131459625897a9195b8f5e468f9bf6856a1
amitu <amitu@cmj-madhuri.cdacmumbai.in>
2021-02-26 11:40:33 +0530

    implemented versioning and security

---
+++ src/main/java/com/amit/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,24 @@
+package com.amit.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV1 {
+     
+	private String name;
+
+	public PersonV1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersonV1(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
